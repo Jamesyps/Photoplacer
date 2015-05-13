15ee5ffef5757cccc27e874905e0fe0b30c89b47
James Wigger <james.s.wigger@gmail.com>
2015-05-13 18:03:56 +0100

    moved imagebank to storage

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -13,14 +13,19 @@ class ImageController extends Controller {
         $width = (int) isset($sizes[0]) ? $sizes[0] : 800;
         $height = (int) isset($sizes[1]) ? $sizes[1] : 600;
 
+        var_dump($this->fetchImages());
     }
 
     private function fetchImages($category = '*')
     {
+        $imagebankPath = storage_path('app/imagebank');
+
         if($category === '*')
         {
 
         }
+
+        return $imagebankPath;
     }
 
 }
