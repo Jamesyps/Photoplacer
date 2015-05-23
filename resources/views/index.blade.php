3df0408a9f230e8997fbd976559bbb3899fc2bb0
James Wigger <james.s.wigger@googlemail.com>
2015-05-23 10:56:59 +0100

    Added check for non numeric values in size inputs

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -105,6 +105,35 @@
     <!-- Scripts -->
     <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
     <script>
+        // Thank you - http://weblog.west-wind.com/posts/2011/Apr/22/Restricting-Input-in-HTML-Textboxes-to-Numeric-Values
+        jQuery.fn.forceNumeric = function () {
+
+            return this.each(function () {
+                $(this).keydown(function (e) {
+                    var key = e.which || e.keyCode;
+
+                    if (!e.shiftKey && !e.altKey && !e.ctrlKey &&
+                                // numbers
+                            key >= 48 && key <= 57 ||
+                                // Numeric keypad
+                            key >= 96 && key <= 105 ||
+                                // comma, period and minus, . on keypad
+                            key == 190 || key == 188 || key == 109 || key == 110 ||
+                                // Backspace and Tab and Enter
+                            key == 8 || key == 9 || key == 13 ||
+                                // Home and End
+                            key == 35 || key == 36 ||
+                                // left and right arrows
+                            key == 37 || key == 39 ||
+                                // Del and Ins
+                            key == 46 || key == 45)
+                        return true;
+
+                    return false;
+                });
+            });
+        };
+
         (function($) {
 
             // Urls
@@ -123,8 +152,8 @@
             var categoryVal = '';
             var filterVal = '';
 
-            $width.on('keyup', setWidth);
-            $height.on('keyup', setHeight);
+            $width.forceNumeric().on('keyup', setWidth);
+            $height.forceNumeric().on('keyup', setHeight);
             $category.on('change', setCategory);
             $filter.on('change', setFilter);
 
